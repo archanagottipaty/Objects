6e8aa2bcfeffcccce5c8634cdd8fdc7b783ebf3a
Archana Gottipaty <archana.gottipaty@gmail.com>
2020-12-16 16:54:14 -0800

    first commit

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,11 @@
+// TODO: Write code to define and export the Employee class
+function Employee(first, last, age, eyecolor) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eyecolor;
+  }
+  
+  Person.prototype.name = function() {
+    return this.firstName + " " + this.lastName;
+  };
